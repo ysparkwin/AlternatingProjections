81eefac44f52e8ad7ea18cf75c45fc248496ab39
YONGSUNG PARK <52879020+ysparkwin@users.noreply.github.com>
2022-06-08 14:40:02 -0700

    Add files via upload

--- _common/errorDOAcutoff.m
+++ _common/errorDOAcutoff.m
@@ -1,20 +1,24 @@
 function [err] = errorDOAcutoff(theEst,theTrue,cutoff)
-err = zeros(size(theTrue));
+
+if numel(theTrue) > 10
+    disp('Too many true elements for this code.')
+    return;
+end
 
 if size(theEst,1)<size(theEst,2), theEst = theEst.'; end
 if size(theTrue,1)<size(theTrue,2), theTrue = theTrue.'; end
 
-theE = theEst;
 if numel(theEst)<numel(theTrue)
-    theE = repmat(theE,ceil(numel(theTrue)/numel(theEst)),1);
+    theEst = [theEst;-inf*ones(ceil(numel(theTrue)/numel(theEst))-1,1)];
 end
-theT = theTrue;
 
-Ap = (repmat(theT',numel(theE),1)-repmat(theE,1,numel(theT)));
-for i=1:numel(theTrue)
-    [ind1,ind2] = find(abs(Ap)==min(min(abs(Ap))));
-%     err(i) = Ap(ind1(1),ind2(1));
-    err(i) = min(abs(Ap(ind1(1),ind2(1))),cutoff);
-    
-    Ap(ind1(1),:) = []; Ap(:,ind2(1)) = [];
+caseListTmp = perms(1:numel(theEst));
+caseList    = unique(caseListTmp(:,1:numel(theTrue)),'rows');
+
+errTmp = theEst(caseList) - repmat(theTrue.',[size(caseList,1),1]);
+errTmp(abs(errTmp)>cutoff) = cutoff;
+
+[~, Loc] = min(sum(power(errTmp,2),2));
+err = abs(errTmp(Loc(1),:).');
+
 end
